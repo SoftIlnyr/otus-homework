6614cf7211b23a2a7905701b936f8778f1d5e1f6
Ilnur Nasibullin <Ilnur.Nasibullin@x5.ru>
2023-09-15 22:37:35 +0300

    hw01.1 Новая версия проекта на gradle kotlon

---
+++ hw12-cash-machine/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "com.softi"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+
+    testImplementation ("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ hw12-cash-machine/src/main/java/com/softi/cash_machine/BanknoteServiceImpl.java
@@ -0,0 +1,46 @@
+package com.softi.cash_machine;
+
+import lombok.Setter;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class BanknoteServiceImpl implements BanknoteService {
+
+    @Setter
+    private Map<BanknoteType, Integer> banknoteCellMap;
+    @Setter
+    private BanknoteSearchService banknoteSearchService;
+
+    public BanknoteServiceImpl() {
+        banknoteCellMap = new TreeMap<>(Comparator.comparing(BanknoteType::getValue).reversed());
+        for (BanknoteType banknoteType : BanknoteType.values()) {
+            banknoteCellMap.put(banknoteType, 0);
+        }
+    }
+
+    @Override
+    public int putMoney(Map<BanknoteType, Integer> banknotes) {
+        int sum = 0;
+
+        for (BanknoteType banknoteType : banknotes.keySet()) {
+            sum += banknoteType.getValue() * banknotes.get(banknoteType);
+            banknoteCellMap.put(banknoteType, banknoteCellMap.get(banknoteType) + banknotes.get(banknoteType));
+        }
+
+        return sum;
+    }
+
+    @Override
+    public Map<BanknoteType, Integer> getBanknotes(int sum) {
+
+        Map<BanknoteType, Integer> availableVariantMap = banknoteSearchService.searchBanknoteBundle(banknoteCellMap, sum);
+
+        if (availableVariantMap.isEmpty()) {
+            throw new IllegalStateException("Невозможно выдать данную сумму");
+        }
+
+        return availableVariantMap;
+    }
+}
